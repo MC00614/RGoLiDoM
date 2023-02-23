c232a293e2a01a688cb076ac2c8f8c21297b0035
jjangddung <jdm0616@naver.com>
2023-02-24 00:41:48 +0900

    Update 2981.py

--- 100JUNE/230128/2981.py
+++ 100JUNE/230128/2981.py
@@ -5,20 +5,25 @@ list1 = []
 for _ in range(n) :
     list1.append(int(input()))
 list1.sort()
-list2 = list1[1:]
 list3= []
-for j in list2 :
-    j = j - list1[0]    
-    list3.append(j)
+for j in range(1, n) :
+    list3.append(list1[j]-list1[j-1])
  
 def GCDofTwoNumbers(a, b): 
     while b != 0 : 
         a, b = b, a%b 
     return a 
- 
 GCDarr = list3[0] 
 for i in range(len(list3)): 
     GCDarr = GCDofTwoNumbers(GCDarr, list3[i])
-for l in range(2,GCDarr+1) :
+new = []
+for l in range(2,int(GCDarr**0.5)+1) :
     if GCDarr % l == 0 :
-        print(l, end= ' ')
+        new.append(l)
+        if(l**2) != GCDarr :
+            new.append(GCDarr//l)     
+new.append(GCDarr)  
+new.sort()
+for q in new :
+    print(q, end = ' ')
+
